ba20f7a30eadf16d8b78a2bbab7d1c523b1b7a6a
Manuj Jain <manuj.ug20@nsut.ac.in>
2023-12-09 21:29:00 +0530

    go-tutorial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GO-TUTORIAL
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main(){
+    fmt.Println("hello");
+	var b=23;
+x  :=23;
+  fmt.Printf("%v",x);
+  fmt.Printf("%T",x);
+  x=34;
+  fmt.Scan(&x);
+  fmt.Println(x);
+	fmt.Println(b);
+}
+
